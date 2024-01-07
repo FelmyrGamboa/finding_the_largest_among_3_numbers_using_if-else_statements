458fefe75714f6515985b8250582a30f8db284b0
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-08 01:01:25 +0800

    Major Revisions in the if-else statements to satisfy what is needed

--- finding_the_largest_among_3_numbers.py
+++ finding_the_largest_among_3_numbers.py
@@ -11,62 +11,24 @@ first_number = float(input("Please input your 1st Number: "))
 second_number = float(input("Please input your 2nd Number: "))
 third_number = float(input("Please input your 3rd Number: "))
 
-#check for the larger number in the three numbers collected
-if first_number > second_number:
-    if first_number > third_number:
-        largest_number = first_number
-    else:
-        if second_number > third_number:
-            largest_number = second_number
-
-if second_number > first_number:
-    if second_number > third_number:
-        largest_number = second_number
-    else:
-        if third_number > first_number:
-            largest_number = third_number
-
-if third_number > second_number:
-    if third_number > first_number:
-        largest_number = third_number
-    else:
-        if first_number > second_number:
-            largest_number = third_number
-
-if first_number == second_number:
-    if first_number > third_number or second_number > third_number:
-        largest_number = first_number or second_number
-    else:
-        largest_number = third_number
-
-if second_number == third_number:
-    if second_number > first_number or third_number > first_number:
-        largest_number = third_number or second_number
-    else:
-        largest_number = first_number
-
-if first_number == third_number:
-    if first_number > second_number or third_number > second_number:
-        largest_number = first_number or third_number
-    else:
-        largest_number = second_number
-
 #check if the numbers are equal to each other
-if first_number == second_number and first_number == third_number and second_number == third_number:
+if first_number == second_number == third_number:
     equal_numbers = print(f"The numbers {first_number}, {second_number} and {third_number} are equal to each other.")
-    exit()
 
-#print the largest number among the 3
-print(f"Among the numbers {first_number}, {second_number} and {third_number}, the largest is {largest_number}")
+#check for the largest number in the three numbers collected
+else:
+    largest_number = max(first_number, second_number, third_number)
+    print(f"Among the numbers {first_number}, {second_number} and {third_number}, the largest is {largest_number}")
 
-def user_input_three_numbers():
-    simpledialog(first_number)
-    simpledialog(second_number)
-    simpledialog(third_number)
+    def user_input_three_numbers():
+        
+        simpledialog(first_number)
+        simpledialog(second_number)
+        simpledialog(third_number)
 
-    biggest = largest_number
+        # biggest = largest_number
 
-    notice = messagebox.showinfo("Result", equal_numbers)
+        messagebox.showinfo("Result", equal_numbers)
 
 root = tk.Tk()
 root.geometry("1280x720")

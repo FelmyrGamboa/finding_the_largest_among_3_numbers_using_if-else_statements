9a97cd4b9eb91b0175454f463f04684909c1ebd7
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-08 00:18:39 +0800

    Tentative if-else statements for finding the largest number (Possible errors may rise)

--- finding_the_largest_among_3_numbers.py
+++ finding_the_largest_among_3_numbers.py
@@ -2,6 +2,54 @@
 
 #pseudocode
 #ask user for input
-first_number = input("Please input your 1st Number: ")
-second_number = input("Please input your 2nd Number: ")
-third_number = input("Please input your 3rd Number: ")
+first_number = float(input("Please input your 1st Number: "))
+second_number = float(input("Please input your 2nd Number: "))
+third_number = float(input("Please input your 3rd Number: "))
+
+#check for the larger number in the three numbers collected
+if first_number > second_number:
+    if first_number > third_number:
+        largest_number = first_number
+    else:
+        if second_number > third_number:
+            largest_number = second_number
+
+if second_number > first_number:
+    if second_number > third_number:
+        largest_number = second_number
+    else:
+        if third_number > first_number:
+            largest_number = third_number
+
+if third_number > second_number:
+    if third_number > first_number:
+        largest_number = third_number
+    else:
+        if first_number > second_number:
+            largest_number = third_number
+
+if first_number == second_number:
+    if first_number > third_number or second_number > third_number:
+        largest_number = first_number or second_number
+    else:
+        largest_number = third_number
+
+if second_number == third_number:
+    if second_number > first_number or third_number > first_number:
+        largest_number = third_number or second_number
+    else:
+        largest_number = first_number
+
+if first_number == third_number:
+    if first_number > second_number or third_number > second_number:
+        largest_number = first_number or third_number
+    else:
+        largest_number = second_number
+
+#check if the numbers are equal to each other
+if first_number == second_number and first_number == third_number and second_number == third_number:
+    print(f"The numbers {first_number}, {second_number} and {third_number} are equal to each other.")
+    exit()
+
+#print the largest number among the 3
+print(f"Among the numbers {first_number}, {second_number} and {third_number}, the largest is {largest_number}")

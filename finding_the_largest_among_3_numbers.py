fd52e3cdb74c34e558334d9ef40a6076ee4e2f3f
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-08 00:35:48 +0800

    Preparing the display and editing of the conditions

--- finding_the_largest_among_3_numbers.py
+++ finding_the_largest_among_3_numbers.py
@@ -1,6 +1,11 @@
 # Assignment#4: Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
 
 #pseudocode
+import tkinter as tk 
+from tkinter import simpledialog
+from tkinter import messagebox
+from PIL import ImageTk, Image
+
 #ask user for input
 first_number = float(input("Please input your 1st Number: "))
 second_number = float(input("Please input your 2nd Number: "))
@@ -48,8 +53,24 @@ if first_number == third_number:
 
 #check if the numbers are equal to each other
 if first_number == second_number and first_number == third_number and second_number == third_number:
-    print(f"The numbers {first_number}, {second_number} and {third_number} are equal to each other.")
+    equal_numbers = print(f"The numbers {first_number}, {second_number} and {third_number} are equal to each other.")
     exit()
 
 #print the largest number among the 3
 print(f"Among the numbers {first_number}, {second_number} and {third_number}, the largest is {largest_number}")
+
+def user_input_three_numbers():
+    simpledialog(first_number)
+    simpledialog(second_number)
+    simpledialog(third_number)
+
+    biggest = largest_number
+
+    notice = messagebox.showinfo("Result", equal_numbers)
+
+root = tk.Tk()
+root.geometry("1280x720")
+root.title("wala pa title wahahaha")
+root.config(bg= "#FDF5DF")
+
+root.mainloop()

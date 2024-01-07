871348ca7576988a6d10b6afc03a66922c535d59
FelmyrGamboa <felmyrgamboa23@gmail.com>
2024-01-07 18:21:17 +0800

    Pseudocode and Ask user for input

--- finding_the_largest_among_3_numbers.py
+++ finding_the_largest_among_3_numbers.py
@@ -1,2 +1,7 @@
 # Assignment#4: Ask user to input 3 numbers. Find and print the biggest number using only if-else statement
 
+#pseudocode
+#ask user for input
+first_number = input("Please input your 1st Number: ")
+second_number = input("Please input your 2nd Number: ")
+third_number = input("Please input your 3rd Number: ")
